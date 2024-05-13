ebb8668ae49590f4ba024516e274d1d8fb01893f
Noah Feng <2027283767@qq.com>
2024-05-13 12:58:19 +0800

    Add files via upload

---
+++ AOOP_WORK/src/Controller/NumberleController.java
@@ -0,0 +1,62 @@
+package Controller;
+
+import Model.INumberleModel;
+import View.NumberleView;
+
+import java.util.ArrayList;
+
+// Controller.NumberleController.java
+public class NumberleController {
+    private INumberleModel model;
+    private NumberleView view;
+    private Boolean ENABLE;
+
+    public NumberleController(INumberleModel model) {
+        this.model = model;
+    }
+
+    public void setView(NumberleView view) {
+        this.view = view;
+    }
+
+    public void processInput(String input) {
+        model.processInput(input);
+    }
+
+    public boolean isGameOver() {
+        return model.isGameOver();
+    }
+
+    public boolean isGameWon() {
+        return model.isGameWon();
+    }
+
+    public String getTargetWord() {
+        return model.getTargetNumber();
+    }
+
+    public StringBuilder getCurrentGuess() {
+        return model.getCurrentGuess();
+    }
+
+    public int getRemainingAttempts() {
+        return model.getRemainingAttempts();
+    }
+
+    public void startNewGame() {
+        model.startNewGame();
+        ENABLE = false;
+    }
+
+    public Boolean getENABLE() {
+        return ENABLE;
+    }
+
+    public void setDISABLE(Boolean disable) {
+        this.ENABLE = disable;
+    }
+
+    public boolean getViewStatus() {
+        return view != null;
+    }
+}
